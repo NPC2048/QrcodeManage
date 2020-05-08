362b138d10004c97dd2c13a4b7a7b63595e18d39
easytone <npc2048@163.com>
2020-05-08 18:07:38 +0800

    .

--- src/main/java/com/liangyuelong/qrcode/common/constant/DateFormatConstants.java
+++ src/main/java/com/liangyuelong/qrcode/common/constant/DateFormatConstants.java
@@ -17,5 +17,26 @@ public class DateFormatConstants {
 
     public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
 
+    public final static String SHORT_DATE = "yyyyMMdd";
+    public final static String O_SHORT_DATE = "MM/dd/yyyy";
+    public final static String O_LONG_DATE = "MM/dd/yyyy HH:mm:ss";
+    public final static String V_LONG_DATE_2 = "yyMMddhhmmss";
+    public final static String V_LONG_DATE_3 = "yyyyMMddhhmmss";
+    public final static String V_LONG_DATE_4 = "yyyyMMddhhmmssSSSSSS";
+    public final static String V_LONG_DATE_5 = "yyyyMMddHHmmssSSS";
+    public final static String V_LONG_YMD = "yyyyMMdd";
+    public final static String V_SHORT_YM = "yyyyMM";
+    public final static String V_SHORT_DATE2 = "yyyy年MM月dd";
+    public final static String V_SHORT_DATE3 = "yyyy年MM月dd日";
+    public final static String V_SHORT_DATE4 = "yyyy-MM-dd HH";
+    public final static String MMDD = "MMdd";
+    public final static String yyyy = "yyyy";
+    public final static String MM = "MM";
+    public final static String dd = "dd";
+    public final static String HH_MM_SS_1 = "HHmmss";
+    /**
+     * C colon, 表示冒号 S small 表示小写
+     */
+    public final static String HHCSMM = "HH:mm";
 
 }
